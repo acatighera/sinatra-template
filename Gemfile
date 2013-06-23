73f942b62fe9a34583dad70cdfdf42322a3fc1f8
Alexandru Catighera <acatighera@gmail.com>
2013-06-23 01:03:16 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source "http://rubygems.org"
+
+gem 'activerecord', '3.2.13', :require => 'active_record'
+gem 'authlogic', '3.3.0'
+gem 'haml', '4.0.3'
+gem 'mysql2', '0.3.11'
+gem 'sinatra', '1.4.2', :require => 'sinatra/base'
+gem 'sass', '3.2.9'
+gem 'sinatra-assetpack', :require => 'sinatra/assetpack'
+gem 'sinatra-contrib', '1.4.0'
+gem 'thin', '1.5.1'
+
+group :development do 
+  gem 'debugger'
+end
---
+++ app.rb
@@ -0,0 +1,33 @@
+require './bootstrap'
+
+class AppName < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+  set :root, File.dirname(__FILE__)
+
+  register Sinatra::AssetPack
+
+  assets {
+    serve '/js',     from: 'assets/js'
+    serve '/css',    from: 'assets/css'
+    serve '/img',    from: 'assets/images'
+
+    js :app, [
+      '/js/vendor/jquery-2.0.2.min.js',
+      '/js/vendor/**/*.js',
+      '/js/vendor/*.js',
+      '/js/lib/**/*.js',
+      '/js/lib/*.js'
+    ]
+
+    css :application, '/css/application.css', [
+      '/css/screen.css',
+      '/css/vendor/*.css',
+      '/css/vendor/**/*.css'
+    ]
+
+    js_compression  :jsmin 
+    css_compression :simple
+  }
+end
