eb214118012cb94d241abc7ff3845d11136f8173
Alex Catighera <acatighera@gmail.com>
2013-08-07 13:58:01 -0400

    remove code that should not have been included in template

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -1,22 +1,5 @@
-class Hokusai < Sinatra::Base
-  #TODO remove this
-  include Twitter::Autolink
-
+class AppName < Sinatra::Base
   get '/' do
-    @subscriptions = Subscription.all
     haml :index
-  end
-
-  get '/import_searches' do 
-    Twitter.saved_searches.each do |saved_search|
-      Subscription.create(:twitter_id => saved_search[:id], :last_synced => Time.now, :query => saved_search[:query], :name => saved_search[:name]) unless Subscription.find_by_twitter_id(saved_search[:id])
-    end
-    redirect to('/')
-  end
-
-  get '/ch/:id' do
-    @subscription = Subscription.find(params[:id])
-    @tweets = Twitter.search(@subscription.query, :count => 10, :result_type => "recent").results
-    haml :channel, :layout => false
   end
 end
