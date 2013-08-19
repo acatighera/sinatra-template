bd2cb0db9aba32ea535bc9923b79a88ecd823f09
Alexandru Catighera <acatighera@gmail.com>
2013-08-19 13:23:24 -0400

    use singular table names

--- bootstrap.rb
+++ bootstrap.rb
@@ -3,3 +3,4 @@ Bundler.require(:default, ENV['RACK_EN'] || :development)
 
 db_config = YAML::load(File.open(File.expand_path('~/configs/app_name/database.yml')))[(ENV['RACK_ENV'] || :development).to_s]
 ActiveRecord::Base.establish_connection(db_config)
+ActiveRecord::Base.pluralize_table_names = false
